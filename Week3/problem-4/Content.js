f757bdea61297c8d4989de9d417d294de62b63a9
Sara Egan <saraegan13@gmail.com>
2023-10-04 20:06:53 +0100

    completed problem-4 chrome extension

--- Week3/problem-4/Content.js
+++ Week3/problem-4/Content.js
@@ -1,26 +1,56 @@
 //array of images
-let catsImages = [
-    "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-    "https://e3.365dm.com/21/03/768x432/skynews-cats-missing-microchip_5315182.jpg?20210323142004",
-    "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc3836660-7846-11eb-80c3-8cc375faed89.jpg?crop=5729%2C3222%2C187%2C805&resize=1200",
-    "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/petting_pet_cat-1296x728-header.jpg?w=1155&h=1528",
-	"https://lh3.googleusercontent.com/pw/AL9nZEXYJlrVkYoKIkpx07_3F4HOiTiOheaoaiRAcwrUg3C613-jkzEubJ3k8Z9fDjG5IfVqCzorphZ00vp6mIyB79GtCsoyV69xXe9cqrA0zglgrcvYhH2UP4cDR88WTm1AmuyCxQHAWCB5JzKD7eD94dtNZA=w690-h920-no"
+let sanrioImages = [
+    "https://www.sanrio.com/cdn/shop/files/ecomm-CHGAL-Core2021_367x353px_07-CN_1000x.png?v=1614324462",
+    "https://i0.wp.com/blog.govoyagin.com/wp-content/uploads/2019/09/%E3%80%8C%E5%A4%8F%E3%81%B2%E3%82%9A%E3%82%85%E3%83%BC%E3%82%8D2022%EF%BD%9E%E3%81%82%E3%81%AE%E5%A4%8F%E3%82%92%E3%82%82%E3%81%86%E4%B8%80%E5%BA%A6%EF%BD%9E%E3%80%8D%E3%83%92%E3%82%99%E3%82%B7%E3%82%99%E3%83%A5%E3%82%A2%E3%83%AB.jpg?fit=1191%2C842&ssl=1",
+    "https://eu-images.contentstack.com/v3/assets/blt781c383a1983f673/bltfbb32a69051ba343/6467adec43cc392290e3ac7e/hellokittyjazwares.png",
+    "https://wallpapers-clan.com/wp-content/uploads/2022/12/sanrio-pfp-36.jpg",
+	"https://wallpapers.com/images/hd/main-sanrio-characters-7jjpu7c549ruidaf.jpg"
 ];
 
 //reverse through array of images
 //getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
 const imgs = document.getElementsByTagName("img");
 for(let i = 0; i < imgs.length; i++) {
-    const randomImg = Math.floor(Math.random() * catsImages.length)
-    imgs[i].src = catsImages[randomImg]
+    const randomImg = Math.floor(Math.random() * sanrioImages.length)
+    imgs[i].src = sanrioImages[randomImg]
 }
 //do the same for h1 elements
 const headers = document.getElementsByTagName("h1");
 for (let i = 0; i < headers.length; i++){
-    headers[i].innerText = "Cats are awesome.";
+    headers[i].innerText = "HELLO KITTY IS THE BEST.";
 }
 //do the same for p elements
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++){
-    p[i].innerText = "This website is now about cats.";
+    p[i].innerText = "MEOW MEOW MEOW MEOW";
+}
+
+//change the background colour of the body
+document.body.style.backgroundColor = 'pink';
+
+ //change the font and colour of all <p> elements
+ const paragraphs = document.querySelectorAll('p');
+ paragraphs.forEach(paragraph => {
+   paragraph.style.fontFamily = 'Cursive, Lucida Handwriting'; // Change the font family to your desired font
+   paragraph.style.color = 'pink';
+ });
+
+  //change the font and colour of all <body> elements
+  const bodies = document.querySelectorAll('body');
+  bodies.forEach(body => {
+    body.style.fontFamily = 'Fantasy, Copperplate'; // Change the font family to your desired font
+    body.style.color = 'yellow';
+  });
+
+ //change the background colour of a <div> element with id "div"
+const subnav = document.getElementById('subnav');
+subnav.style.backgroundColor = 'lime';
+
+//the link that all links will now bring you to
+var newLink = 'https://www.sanrio.com/';
+//get all the a elements
+var allLinks = document.querySelectorAll('a');
+//go through every link
+for (var i = 0; i < allLinks.length; i++) {
+  allLinks[i].href = newLink;
 }
---
+++ Week3/problem-4/README.md
@@ -0,0 +1,9 @@
+# My Web Extension
+
+This extension will change all images to a variation of hello kitty images
+Will change all h1 elements to say hello kitty is the best
+Will change all p elements to say MEOW MEOW MEOW MEOW
+Will change the background colour to pink
+Will change the font and colour of all p and body elements
+Will change the background colours of div elements
+All a elements will bring you to the hello kitty website
