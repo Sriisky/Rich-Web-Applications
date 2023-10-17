7de0efe3c81122cb2b8544a028458d7a13854a15
Sara Egan <saraegan13@gmail.com>
2023-10-17 15:48:19 +0100

    Problem 3 in progress. need to fix styling still

---
+++ Week4/Problem3/script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const GithubUsername = document.getElementById("GithubUsername");
+    const searchButton = document.getElementById("searchButton");
+    const userInfo = document.getElementById("userInfo");
+    const repoList = document.getElementById("repoList");
+
+    searchButton.addEventListener("click", () => {
+        const username = GithubUsername.value.trim();
+        if (username === "") {
+            alert("You must enter a GitHub GithubUsername");
+            return;
+        }
+
+        //make a request to the GitHub API to fetch the users data
+        fetch(`https://api.github.com/users/${username}`)
+            .then((response) => {
+                if (response.status === 404) {
+                    throw new Error("User not found");
+                }
+                return response.json();
+            })
+
+            .then((userData) => {
+                userInfo.innerHTML = `
+                    <h2>User Profile</h2>
+                    <img src="${userData.avatar_url}" alt="Profile picture" width="100">
+                    <p>Name: ${userData.name}</p>
+                    <p>Username: ${userData.login}</p>
+                    <p>Email: ${userData.email || "Not available"}</p>
+                    <p>Location: ${userData.location || "Not available"}</p>
+                    <p>Number of Gists: ${userData.public_gists}</p>
+                `;
+
+                //request user's repositories
+                return fetch(`https://api.github.com/users/${username}/repos`);
+            })
+            .then((response) => response.json())
+            .then((repos) => {
+
+                repoList.innerHTML = "<h2>User Repos</h2>";
+                if (repos.length > 5) {
+                    repoList.style.maxHeight = "200px"; // Set a maximum height
+                    repoList.style.overflowY = "auto";   // Add scrollbar
+                }
+
+                repos.forEach((repo) => {
+                    repoList.innerHTML += `
+                        <p>Name: ${repo.name}</p>
+                        <p>Description: ${repo.description || "No description available"}</p>
+                        <hr>
+                    `;
+                });
+            })
+            
+            .catch((error) => {
+                userInfo.innerHTML = `<p>${error.message}</p>`;
+                repoList.innerHTML = "";
+            });
+    });
+});
