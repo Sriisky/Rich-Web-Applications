bdfc7b807f41abab71cdb3f1c239a5c5448789e4
Sara Egan <saraegan13@gmail.com>
2023-12-09 19:53:52 +0000

    problems completed

--- Week11/README.md
+++ Week11/README.md
@@ -1,5 +1,16 @@
 # Lab 4 - ReactJS Information to Run Code
 
-Node-modules have been removed due to size contraints for submission.
+Node-modules have been removed from submission due to size contraints.
 
 First run - npm start
+then - npm install axios
+then - npm install react datepicker
+
+Problem 1 - Was completed by running npx create-react-app react-note-app and converting Lab 1 Problem 1 into the React project.
+
+Problem 2 - Can be found in src/App.js. I chose to implement the CatFact api to display a cat fact when a button is pressed.
+
+Problem 3 - Additional functionalities include the following. 
+1. A search function that allows users to search for notes based on common letters and words contained within each of the notes. 
+2. The notes can now be refined to show only notes of a given colour, or you can also view all notes. 
+3. A date is allocated to the note which can be changed by clicking the 'edit' button of a note. Can search by dates.
--- Week11/react-note-app/src/App.js
+++ Week11/react-note-app/src/App.js
@@ -1,9 +1,131 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Axios from "axios"; // to make api requests
+import DatePicker from "react-datepicker"; //calender integration
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
-  return (
-    <div className="App">
+  const [notes, setNotes] = useState([]);
+  const [noteText, setNoteText] = useState('');
+  const [noteColor, setNoteColor] = useState('Yellow');
+  const [noteDate, setNoteDate] = useState(new Date());
+  const [catFact, setCatFact] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterColor, setFilterColor] = useState('');
+  const [filterDate, setFilterDate] = useState(null);
 
+  // CAT FACT API FETCH
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
+  const addNote = () => {
+    if (!noteText) return; //dont allow user to add empty notes
+    setNotes([...notes, { text: noteText, color: noteColor, date: noteDate }]);
+    setNoteText('');
+    setNoteDate(new Date());
+  };
+
+  // edit a notes content or date
+  const editNote = (index) => {
+    const newNotes = [...notes];
+    const editedText = prompt('Edit your note:', newNotes[index].text);
+    if (editedText !== null) {
+      newNotes[index].text = editedText;
+      const editedDate = new Date(prompt('Edit the date:', newNotes[index].date));
+      newNotes[index].date = isNaN(editedDate) ? newNotes[index].date : editedDate;
+      setNotes(newNotes);
+    }
+  };
+
+  // delete a note
+  const deleteNote = (index) => {
+    const newNotes = notes.filter((_, i) => i !== index);
+    setNotes(newNotes);
+  };
+
+  // filter notes based on search term, color, and date
+  const filteredNotes = notes.filter(note => {
+    const noteDate = new Date(note.date).setHours(0, 0, 0, 0);
+    const selectedFilterDate = filterDate ? filterDate.setHours(0, 0, 0, 0) : null;
+    return note.text.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (!filterColor || note.color === filterColor) &&
+      (!selectedFilterDate || noteDate === selectedFilterDate);
+  });
+
+  return (
+    <div className="container"> 
+      <h1>Welcome to Your Favourite Note Taking App</h1>
+
+      {/* for each note, put in text, select a colour */}
+      <div className="note-form">
+        <input 
+          type="text"
+          value={noteText}
+          onChange={(e) => setNoteText(e.target.value)}
+          placeholder="Enter your note"
+        />
+
+        <button onClick={addNote}>Create Note</button>
+
+        <select
+          value={noteColor}
+          onChange={(e) => setNoteColor(e.target.value)}>
+          <option value="Yellow">Yellow</option>
+          <option value="Orange">Orange</option>
+          <option value="Lime">Lime</option>
+          <option value="Pink">Pink</option>
+        </select>
+      </div>
+
+      {/* filter by colours or by date or both */}
+      <div className="filter-section">
+        <button onClick={() => setFilterColor('Yellow')}>Yellow Notes</button>
+        <button onClick={() => setFilterColor('Orange')}>Orange Notes</button>
+        <button onClick={() => setFilterColor('Lime')}>Lime Notes</button>
+        <button onClick={() => setFilterColor('Pink')}>Pink Notes</button>
+        <button onClick={() => setFilterColor('')}>All Notes</button>
+
+        <DatePicker
+          selected={filterDate}
+          onChange={(date) => setFilterDate(date)}
+          isClearable
+          placeholderText="Filter by date"/>
+      </div>
+
+      {/* search functionality to find notes containing common letters */}
+      <div className="search-section">
+        <input
+          type="text"
+          placeholder="Search notes..."
+          onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
+
+      {/* note container */}
+      <div className="notes">
+      {filteredNotes.map((note, index) => (
+          <div key={index} className="note" style={{ backgroundColor: note.color }}>
+            <p>{note.text}</p>
+            <p>Date: {note.date.toLocaleDateString()}</p>
+            <div className="note-actions">
+              <button onClick={() => editNote(index)}>Edit</button>
+              <button onClick={() => deleteNote(index)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    
+      {/* display the cat fact */}
+      <div className='catFactSection'>
+        <button onClick={fetchCatFact}>Click to get a new cat fact</button>
+        <p>{catFact}</p>
+      </div>
     </div>
   );
 }
